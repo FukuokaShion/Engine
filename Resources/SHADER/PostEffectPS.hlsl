a385e79cdf82e6a7d536ad0f5e57ff36a8b1b6c2
Fukuoka_Shion <k021g1250@g.neec.ac.jp>
2023-07-21 15:49:41 +0900

    03_08提出用に変更

--- Resources/SHADER/PostEffectPS.hlsl
+++ Resources/SHADER/PostEffectPS.hlsl
@@ -2,7 +2,7 @@
 
 
 Texture2D<float4> tex0 : register(t0);  	// 0番スロットに設定されたテクスチャ
-Texture2D<float4> tex1 : register(t1);  	// 0番スロットに設定されたテクスチャ
+Texture2D<float4> tex1 : register(t1);  	// 1番スロットに設定されたテクスチャ
 SamplerState smp : register(s0);      	// 0番スロットに設定されたサンプラー
 
 float4 main(VSOutput input) : SV_TARGET{
@@ -35,8 +35,8 @@ float4 main(VSOutput input) : SV_TARGET{
 	float4 color = averageColor;
 
 	if (fmod(input.uv.y, 0.1f) < 0.05f) {
-		color = colortex0;
+		color = float4(1, 1, 1, 1) - colortex0;
 	}
 
-	return color;
+	return float4(color.rgb, 1);
 }
